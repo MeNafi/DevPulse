ed3731dccbe7ae49e5e1decc3a47fdb734071c5f
MeNafi <nayeefsarkernafi@gmail.com>
2026-06-07 19:03:40 +0600

    add controller

--- src/middleware/logger.ts
+++ src/middleware/logger.ts
@@ -1,5 +1,6 @@
 import type { NextFunction, Request, Response } from "express";
 
+// Middleware to log incoming HTTP requests with timestamp, method, and URL
 export const logger = (req: Request, res: Response, next: NextFunction) => {
      
     const timestamp = new Date().toISOString()
