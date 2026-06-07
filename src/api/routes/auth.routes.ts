0e58b2fff24aceca46356e84798bd1e8af68659e
MeNafi <nayeefsarkernafi@gmail.com>
2026-06-07 19:04:26 +0600

    add routes

--- src/api/routes/auth.routes.ts
+++ src/api/routes/auth.routes.ts
@@ -1,6 +1,7 @@
 import { Router } from 'express'
 import { signupController, loginController } from '../controllers/auth.controller'
 
+
 const router = Router()
 
 // Handle secure user registration gateway
