778f7fac5581e6894be7891ca538e598acda7c17
Nayeef sarker Nafi <155153560+MeNafi@users.noreply.github.com>
2026-06-12 16:53:32 +0600

    Update the code

--- src/config/index.ts
+++ src/config/index.ts
@@ -5,7 +5,6 @@ import path from "path"
 dotenv.config({path: path.join(process.cwd(), ".env") });
 
 export const config = {
-
     port: process.env.PORT || 5000,
     database_url: process.env.DATABASE_URL as string,
     jwt_secret: process.env.JWT_SECRET as string,
